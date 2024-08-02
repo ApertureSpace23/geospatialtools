fa0a5b51e6e12a659865277b276ececef0e54e2a
Rory Dunn <rory.f.dunn@gmail.com>
2024-08-02 15:56:58 -0400

    trying this

--- setup.py
+++ setup.py
@@ -1,47 +1,29 @@
-import setuptools
-from setuptools import setup, find_packages
-from setuptools.command.build_ext import build_ext
-import os
-import subprocess
+from setuptools import setup, find_packages, Extension
+import numpy
 
-class CustomBuildExtCommand(build_ext):
-    """Custom command to compile Fortran files using f2py before building the package."""
-    def run(self):
-        # Compile Fortran modules
-        f2py_cmds = [
-            ('terrain_tools_fortran', ['src/planchon_2001.f90', 'src/terrain_tools.f90']),
-            ('upscaling_tools_fortran', ['src/upscaling_tools.f90'])
-        ]
-        for mod_name, sources in f2py_cmds:
-            try:
-                # You can customize the f2py command according to your compilation preferences
-                subprocess.check_call(['f2py', '-c', '-m', mod_name] + sources + ['-fPIC', '-Wall', '-pedantic', '-O3'])
-            except subprocess.CalledProcessError as e:
-                raise RuntimeError("f2py failed to compile: " + ' '.join(e.cmd))
-        # Call the original build_ext command
-        super().run()
 
-# Diagnostic print to see what packages find_packages is finding
-print("Packages found:", find_packages(where='libraries'))
+def configuration(parent_package='', top_path=None):
+    config = {
+        'name': 'geospatialtools',
+        'version': '0.1.0',
+        'description': 'A collection of geospatial tools including Fortran extensions.',
+        'author': 'Your Name',
+        'author_email': 'your.email@example.com',
+        'packages': find_packages(),
+        'ext_modules': [
+            Extension('geospatialtools.terrain_tools_fortran',
+                      ['src/planchon_2001.f90', 'src/terrain_tools.f90'],
+                      extra_compile_args=['-fPIC', '-Wall', '-pedantic', '-O3']),
+            Extension('geospatialtools.upscaling_tools_fortran',
+                      ['src/upscaling_tools.f90'],
+                      extra_compile_args=['-fPIC', '-Wall', '-pedantic', '-O3'])
+        ],
+        'package_dir': {'': 'libraries'},
+        'install_requires': [
+            'numpy>=' + numpy.__version__,
+        ],
+    }
+    return config
 
-setup(
-    name="geospatialtools",
-    version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A collection of geospatial tools.",
-    packages=['geospatialtools', 'geospatialtools.libraries'],
-    package_dir={
-        'geospatialtools': 'libraries',  # Point to the 'libraries' directory for the root package
-        'geospatialtools.libraries': 'libraries'  # Sub-packages are also in 'libraries'
-    },
-    install_requires=[
-        "numpy>=1.26.4"  # ensure dependencies are listed correctly
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.12',
-)
+if __name__ == '__main__':
+    setup(**configuration())
